3df00847c1ae5f66b74718cf6f5bc684b0f55c0f
mjiid <habush1610@gmail.com>
2024-06-22 16:30:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='PyCourrier',
-    version='0.2',
+    version='0.2.1',
     packages=find_packages(),
     description='A Python package for sending emails using various SMTP services',
     author='Abdelmajid Habouch',
